42bba30332eacb65547be4f52e1f03f2c0c43510
Michael Pfaff <michael@pfaff.dev>
2025-03-25 21:20:37 -0400

    Remove commented out code for JSON logging

--- src/util/logging.zig
+++ src/util/logging.zig
@@ -18,7 +18,6 @@ export fn load_logger_config() callconv(.c) enum(u8) {
     };
     log_mode = std.StaticStringMap(LogMode).initComptime(.{
         .{ "text", .text },
-        // .{ "json", .json },
     }).get(log_mode_str) orelse {
         logger.err("Unrecognized logger mode: {s}", .{log_mode_str});
         return .err;
@@ -28,31 +27,9 @@ export fn load_logger_config() callconv(.c) enum(u8) {
 
 const LogMode = enum {
     text,
-    // json,
 };
 
 pub var log_mode: LogMode = .text;
-
-// fn JsonStringEscapingWriter(comptime Writer: type) type {
-//     return struct {
-//         delegate: Writer,
-
-//         const WriteError = @typeInfo(@typeInfo(@TypeOf(write)).@"fn".return_type.?).error_union.error_set;
-
-//         pub fn write(self: @This(), bytes: []const u8) !usize {
-//             try std.json.encodeJsonStringChars(bytes, .{}, self.delegate);
-//             return bytes.len;
-//         }
-
-//         pub fn writer(self: @This()) std.io.GenericWriter(@This(), WriteError, write) {
-//             return .{ .context = self };
-//         }
-//     };
-// }
-
-// fn jsonStringEscapingWriter(writer: anytype) JsonStringEscapingWriter(@TypeOf(writer)) {
-//     return .{ .delegate = writer };
-// }
 
 pub fn log(comptime message_level: std.log.Level, comptime scope: @TypeOf(.enum_literal), comptime format: []const u8, args: anytype) void {
     const level_txt = comptime message_level.asText();
@@ -68,21 +45,6 @@ pub fn log(comptime message_level: std.log.Level, comptime scope: @TypeOf(.enum_
             .text => {
                 writer.print(level_txt ++ " " ++ scope_txt ++ " " ++ format ++ "\n", args) catch return;
             },
-            // .json => {
-            //     var stream = std.json.writeStream(writer, .{});
-            //     stream.beginObject() catch return;
-            //     stream.objectFieldRaw("\"level\"") catch return;
-            //     stream.write(message_level) catch return;
-            //     stream.objectFieldRaw("\"scope\"") catch return;
-            //     stream.write(scope) catch return;
-            //     stream.objectFieldRaw("\"message\"") catch return;
-            //     stream.beginWriteRaw() catch return;
-            //     stream.stream.writeByte('"') catch return;
-            //     std.fmt.format(jsonStringEscapingWriter(stream.stream).writer(), format, args) catch return;
-            //     stream.stream.writeByte('"') catch return;
-            //     stream.endWriteRaw();
-            //     stream.endObject() catch return;
-            // },
         }
         bw.flush() catch return;
     }
